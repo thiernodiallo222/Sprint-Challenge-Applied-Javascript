47397778331e103fcd0df8ef93f8ff765a39dd5a
thiernodiallo222 <thiernodiallo864@gmail.com>
2019-12-13 20:05:42 -0500

    Completed first js file

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,37 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    //creating elements
+
+    const Header = document.createElement('div'),
+        Date = document.createElement('span'),
+        LambdaTimes = document.createElement('h1'),
+        Temperature = document.createElement('span');
+
+        // adding contents
+
+    Date.textContent = 'SMARCH 28, 2019';
+    LambdaTimes.textContent = 'Lambda Times';
+    Temperature.textContent = '98°';
+
+    // Adding classes
+
+    Header.classList.add('header');
+    Date.classList.add('date');
+    Temperature.classList.add('temp');
+
+    // Setting up structures
+
+    Header.appendChild(Date);
+    Header.appendChild(LambdaTimes);
+    Header.appendChild(Temperature);
+    
+    return Header;
+}
+
+// Adding it to the DOM
+
+const HeaderContainer = document.querySelector('.header-container');
+HeaderContainer.appendChild(Header());
+
